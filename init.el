6e2ac9ab02f044f810ea77e93ee246b9d6c550b8
Robin <robinkqin@163.com>
2021-03-14 22:30:44 +0800

    MOD: hotkeys

--- init.el
+++ init.el
@@ -100,23 +100,21 @@
 (setq enable-recursive-minibuffers t)
 ;; enable this if you want `swiper' to use it
 ;; (setq search-default-mode #'char-fold-to-regexp)
-;; (global-set-key "\C-s" 'swiper)
-;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
 ;; (global-set-key (kbd "<f6>") 'ivy-resume)
-;; (global-set-key (kbd "M-x") 'counsel-M-x)
-;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;; (global-set-key (kbd "<f5> f") 'counsel-describe-function)
-;; (global-set-key (kbd "<f5> v") 'counsel-describe-variable)
-;; (global-set-key (kbd "<f5> s") 'counsel-describe-symbol)
-;; (global-set-key (kbd "<f5> l") 'counsel-find-library)
-;; (global-set-key (kbd "<f5> i") 'counsel-info-lookup-symbol)
-;; (global-set-key (kbd "<f5> u") 'counsel-unicode-char)
-;; (global-set-key (kbd "C-c f") 'counsel-git)
-;; (global-set-key (kbd "C-c g") 'counsel-git-grep)
-;; (global-set-key (kbd "C-c r") 'counsel-rg)
-;; (global-set-key (kbd "C-c k") 'counsel-ag)
-;; (global-set-key (kbd "C-x l") 'counsel-locate)
-;; (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-h o") 'counsel-describe-symbol)
+(global-set-key (kbd "C-h l") 'counsel-find-library)
+(global-set-key (kbd "C-h S") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "C-h u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c f") 'counsel-git)
+(global-set-key (kbd "C-c g") 'counsel-git-grep)
+(global-set-key (kbd "C-c r") 'counsel-rg)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 
 ;;ripgrep
@@ -134,7 +132,7 @@
 ;;projectile
 (projectile-mode +1)
 ;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map) ;; mac os
-;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
 
 
 ;;yasnippet
@@ -157,7 +155,7 @@
 (require 'org)
 (setq org-src-fontify-natively t)
 (setq org-agenda-files '("~/org"))
-;; (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c a") 'org-agenda)
 
 
 ;;which-key
