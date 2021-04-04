21398dc1c5683b9823182f244f1ad5d5b03db614
Robin <robinkqin@163.com>
2021-04-04 10:45:58 +0800

    MOD: ggtags

--- init.el
+++ init.el
@@ -65,6 +65,7 @@
                       counsel
                       counsel-etags
                       counsel-gtags
+                      ggtags
                       swiper
                       rg
                       projectile
@@ -166,6 +167,21 @@
 (require 'counsel-gtags)
 (add-hook 'c-mode-hook 'counsel-gtags-mode)
 (add-hook 'c++-mode-hook 'counsel-gtags-mode)
+
+(require 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode)
+              (ggtags-mode 1))))
+
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
 
 ;;rg
 (require 'rg)
